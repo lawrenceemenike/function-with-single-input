129700522f5423e97ca522cdccd398488da4557a
swerzd <clenmenikev@gmail.com>
2021-03-24 06:24:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Review: 
+# Create a function called greet(). 
+name = input('please what is your name? ')
+def greet(name):
+  print(f'Hello friend {name}')
+  print(f'Sure you are okay {name}?')
+  print(f'just making sure you are {name}')
+
+greet(name)
+
+
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
